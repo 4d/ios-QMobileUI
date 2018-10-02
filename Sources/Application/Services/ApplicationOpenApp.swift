5c115183486023279adeb75de713500836482452
Eric Marchand <eric.marchand@4d.com>
2018-10-02 11:04:25 +0100

    [Mobile] menu with action according to type #100747
    Imported from Git
     Author: Eric Marchand <eric.marchand@4d.com> 1538474658 +0200
     Committer: Eric Marchand <eric.marchand@4d.com> 1538474658 +0200
     sha1: e7330aedb50909d9dda004f373024f575853836d
     push-state: complete
     parent-changes: 34b6f75368092acfc44f8b8f3c78a04272dee58b=[227794]
    
    [git-p4: depot-paths = "//depot/4eDimension/main/QMobile/QMobileUI/": change = 228046]

--- Sources/Application/Services/ApplicationOpenApp.swift
+++ Sources/Application/Services/ApplicationOpenApp.swift
@@ -8,7 +8,38 @@
 
 import Foundation
 
-open class ApplicationOpenAppBeta {
+/// Open native app
+/// https://developer.apple.com/library/archive/featuredarticles/iPhoneURLScheme_Reference/Introduction/Introduction.html
+open class ApplicationOpenAppBeta: NSObject {
+
+    public enum Kind {
+        case safari
+        case map
+        case phone
+        case sms
+        case faceTime
+        case faceTimeAudio
+        case mail
+    }
+
+    public static func open(kind: Kind, string: String) {
+        switch kind {
+        case .safari:
+            ApplicationOpenAppBeta.openSafari(url: string)
+        case .map:
+            ApplicationOpenAppBeta.openMap(address: string)
+        case .phone:
+            ApplicationOpenAppBeta.openPhone(phone: string)
+        case .sms:
+            ApplicationOpenAppBeta.openSMS(phone: string)
+        case .faceTime:
+            ApplicationOpenAppBeta.openFaceTime(call: string)
+        case .faceTimeAudio:
+            ApplicationOpenAppBeta.openFaceTimeAudio(call: string)
+        case .mail:
+            ApplicationOpenAppBeta.openMail(mailto: string)
+        }
+    }
 
     public static func openSafari(url: String) {
         self.openURL(url.trimmed)
@@ -19,7 +50,11 @@ open class ApplicationOpenAppBeta {
     }
 
     public static func openMap(address: String) {
-        self.openURL("http://maps.apple.com/?address=\(address)")
+        self.openURL("http://maps.apple.com/?address=\(address.noNewLines.queryEncoded)")
+    }
+
+    public static func openMap(destination: String) {
+        self.openURL("http://maps.apple.com/?daddr=\(destination.noNewLines.queryEncoded)")
     }
 
     public static func openMap(location: String) {
@@ -93,4 +128,112 @@ extension String {
     var noSpace: String {
        return self.components(separatedBy: .whitespacesAndNewlines).joined()
     }
+    var noNewLines: String {
+        return self.components(separatedBy: .newlines).joined(separator: " ")
+    }
+    var queryEncoded: String {
+        return self.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? self
+    }
 }
+
+extension ApplicationOpenAppBeta {
+
+    fileprivate static func controller(_ title: String, _ message: String, _ actions: [UIAlertAction]) -> UIAlertController {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .actionSheet)
+        for action in actions {
+            alertController.addAction(action)
+        }
+
+        // custom header view
+       /* let margin: CGFloat = 10.0
+        let rect = CGRect(x: margin, y: margin, width: alertController.view.bounds.size.width - margin * 4.0, height: 50)
+        let customView = UIView(frame: rect)
+
+        customView.backgroundColor = .green
+        alertController.view.addSubview(customView)*/
+
+        return alertController
+    }
+
+    public static func alertAddress(_ address: String) {
+        var actions: [UIAlertAction] = []
+
+        actions.append(UIAlertAction(title: "Get Directions", style: .default) { _ in
+            ApplicationOpenAppBeta.openMap(destination: address)
+        })
+        actions.append(UIAlertAction(title: "Open in Maps", style: .default) { _ in
+            ApplicationOpenAppBeta.openMap(address: address)
+        })
+        /*actions.append(UIAlertAction(title: "Add to Contacts", style: .default) { _ in
+
+        })*/
+        actions.append(UIAlertAction(title: "Copy Address", style: .default) { _ in
+            UIPasteboard.general.string = address
+        })
+        actions.append(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+
+        let title = address
+        let message = ""
+
+        controller(title, message, actions).presentOnTop()
+    }
+
+    public static func alertPhone(_ phone: String) {
+        var actions: [UIAlertAction] = []
+
+        actions.append(UIAlertAction(title: "Call", style: .default) { _ in
+            ApplicationOpenAppBeta.openPhone(phone: phone)
+        })
+        actions.append(UIAlertAction(title: "Message", style: .default) { _ in
+            ApplicationOpenAppBeta.openSMS(phone: phone)
+        })
+        actions.append(UIAlertAction(title: "FaceTime", style: .default) { _ in
+            ApplicationOpenAppBeta.openFaceTime(call: phone)
+        })
+        actions.append(UIAlertAction(title: "Copy Phone Number", style: .default) { _ in
+            UIPasteboard.general.string = phone
+        })
+        /*actions.append(UIAlertAction(title: "Add to Existing Contact", style: .default) { _ in
+
+         })*/
+        /*actions.append(UIAlertAction(title: "Create New contact", style: .default) { _ in
+
+         })*/
+        actions.append(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+
+        let title = ""
+        let message = phone
+
+        controller(title, message, actions).presentOnTop()
+    }
+
+}
+
+/*
+import Contacts
+import ContactsUI
+extension ApplicationOpenAppBeta: CNContactViewControllerDelegate {
+
+    func addPhoneNumber(phone: String) {
+        let store = CNContactStore()
+        let contact = CNMutableContact()
+        let homePhone = CNLabeledValue(label: CNLabelHome, value: CNPhoneNumber(stringValue: phone))
+        contact.phoneNumbers = [homePhone]
+        let controller = CNContactViewController(forUnknownContact: contact)
+        controller.contactStore = store
+        controller.delegate = self
+
+        //navigationController.setNavigationBarHidden(false, animated: true)
+        //navigationController.pushViewController(controller, animated: true)
+    }
+
+    public func contactViewController(_ viewController: CNContactViewController, shouldPerformDefaultActionFor property: CNContactProperty) -> Bool {
+        return true
+    }
+
+    public func contactViewController(_ viewController: CNContactViewController, didCompleteWith contact: CNContact?) {
+
+    }
+
+}
+*/
