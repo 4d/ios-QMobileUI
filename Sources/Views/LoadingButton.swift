31ab8ff1865eb3f064a1b5f469c5963f8d6c3004
Eric Marchand <eric.marchand@4d.com>
2018-11-22 10:52:16 +0100

    [Mobile] Fix authentication loading button missing text when clicking fastly
    Imported from Git
     Author: Eric Marchand <eric.marchand@4d.com> 1542880328 +0100
     Committer: Eric Marchand <eric.marchand@4d.com> 1542880328 +0100
     sha1: 957fb6fc77d2622c3363a399eb56d8b59cf7995e
     push-state: complete
     parent-changes: 43c4fb671ee0ef3bffe2a395d9c4439483ef0598=[229891]
    
    [git-p4: depot-paths = "//depot/4eDimension/main/QMobile/QMobileUI/": change = 230017]

--- Sources/Views/LoadingButton.swift
+++ Sources/Views/LoadingButton.swift
@@ -81,7 +81,10 @@ open class LoadingButton: UIButton, UIViewControllerTransitioningDelegate {
     open func startAnimation() {
         // Remove the title
         let state: UIControl.State = .normal
-        self.cachedTitle = title(for: state)
+        let titleForState = title(for: state)
+        if !titleForState.isEmpty {
+            self.cachedTitle = titleForState
+        }
         self.setTitle("", for: state)
 
         // Animate radius
