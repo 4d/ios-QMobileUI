1d1e58530069adad3420a646f8182fc9f364f38f
Eric Marchand <eric.marchand@4d.com>
2018-12-12 09:21:00 +0100

    [Mobile] Fix app settings reset by not checking if setting preferences is immutable or not #103947 but use instead a class property for settings
    Imported from Git
     Author: Eric Marchand <eric.marchand@4d.com> 1544602853 +0100
     Committer: Eric Marchand <eric.marchand@4d.com> 1544602853 +0100
     sha1: c7ac9d4a4238501935b063e91f7946ac496a1618
     push-state: complete
     parent-changes: ae9c636bf275b6642fe67752c4730a087501b9d8=[231024]
    
    [git-p4: depot-paths = "//depot/4eDimension/main/QMobile/QMobileUI/": change = 231079]

--- Sources/Application/Services/ApplicationPreferences.swift
+++ Sources/Application/Services/ApplicationPreferences.swift
@@ -13,22 +13,40 @@ import QMobileAPI
 
 class ApplicationPreferences: NSObject {}
 
+private let kUUIDKey = "uuid"
 extension ApplicationPreferences: ApplicationService {
 
     static var instance: ApplicationService = ApplicationPreferences()
-    static let uuidKey = "uuid"
 
-    static let preferences: MutableCompositePreferences = {
+    static let settings: PreferencesType = {
         #if DEBUG
         // if compiled without debug, this file will not be used
         let settings: CompositePreferences = [Plist(filename: "Settings.debug") ?? [:], Plist(filename: "Settings") ?? [:]]
         #else
         let settings: DictionaryPreferences = Plist(filename: "Settings") ?? [:]
         #endif
-        let preferences: MutableCompositePreferences = [Foundation.UserDefaults.standard, settings, Bundle.main]
+        return settings
+    }()
+
+    static let preferences: MutableCompositePreferences = {
+        let preferences: MutableCompositePreferences = [Foundation.UserDefaults.standard, ApplicationPreferences.settings, Bundle.main]
         Prephirences.sharedInstance = preferences
         return preferences
     }()
+
+    fileprivate func resetSettings() {
+        let userDefaults = Foundation.UserDefaults.standard
+        logger.info("Reset settings")
+        userDefaults.clearAll()
+        userDefaults.synchronize()
+        // remove also keychain.
+        if Prephirences.Auth.Logout.atStart {
+            Prephirences.Auth.Logout.token = APIManager.instance.authToken?.token // keep a token for logout
+        }
+        let keyChain = KeychainPreferences.sharedInstance
+        keyChain.clearAll() // keyChain.lastStatus allow to see that not work
+        APIManager.removeAuthToken()
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) {
         let preferences = ApplicationPreferences.preferences
@@ -36,28 +54,24 @@ extension ApplicationPreferences: ApplicationService {
 
         // Fix simulator bug which keep user default for old application
         let userDefaults = Foundation.UserDefaults.standard
-        let uuidKey = ApplicationPreferences.uuidKey
-        let settings = CompositePreferences(preferences.immutableArray)
-        if let uuid = userDefaults[uuidKey] as? String {
-            if uuid != settings[uuidKey] as? String {
-                resetDefaults = true // and uuid in pref, but not same as settings file -> reset
+        let settings = ApplicationPreferences.settings
+        if let expectedUUID = settings[kUUIDKey] as? String {
+            if let uuid = userDefaults[kUUIDKey] as? String {
+                if uuid != expectedUUID {
+                    logger.debug("Application will reset settings because current uuid \(uuid) not equals to expected one \(expectedUUID)")
+                    resetDefaults = true // and uuid in pref, but not same as settings file -> reset
+                }
+            } else {
+                logger.debug("No uuid yet. Reset settings and set uuid \(expectedUUID)")
+                resetDefaults = true // no uuid in pref -> reset
             }
-        } else {
-            resetDefaults = true // no uuid in pref -> reset
-        }
+        } // else no uuid for app, cannot presume
+
         if resetDefaults {
-            logger.info("Reset settings")
-            userDefaults.clearAll()
-            userDefaults.synchronize()
-            // remove also keychain.
-            if Prephirences.Auth.Logout.atStart {
-                Prephirences.Auth.Logout.token = APIManager.instance.authToken?.token // keep a token for logout
-            }
-            let keyChain = KeychainPreferences.sharedInstance
-            keyChain.clearAll() // keyChain.lastStatus allow to see that not work
-            APIManager.removeAuthToken() // workaround because keychain is not really removed
+            resetSettings() // workaround because keychain is not really removed
         }
-        userDefaults[uuidKey] = settings[uuidKey]
+        // Set new uuid
+        userDefaults[kUUIDKey] = settings[kUUIDKey]
         userDefaults.synchronize()
     }
 
