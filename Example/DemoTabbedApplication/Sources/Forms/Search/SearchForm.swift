91e713dc4dcb2b6d9ec477b1fba18cc61194bb4b
Eric Marchand <eric.marchand@4d.com>
2019-03-04 15:29:29 +0100

    [Mobile] Enahance License file creation of iOS SDK by filtering and sorting Remove old demo app from QMobileUI
    [git-p4: depot-paths = "//depot/4eDimension/main/QMobile/QMobileUI/": change = 234187]

--- Example/DemoTabbedApplication/Sources/Forms/Search/SearchForm.swift
+++
@@ -1,9 +0,0 @@
-//
-//  SearchForms.swift
-//  DemoTabbedApplication
-//
-//  Created by Eric Marchand on 09/11/2017.
-//  Copyright © 2017 Eric Marchand. All rights reserved.
-//
-
-import UIKit
--- Example/DemoTabbedApplication/Sources/Forms/Web/WebAreaForm.swift
+++
@@ -1,9 +0,0 @@
-//
-//  WebForm.swift
-//  DemoTabbedApplication
-//
-//  Created by Eric Marchand on 17/11/2017.
-//  Copyright © 2017 Eric Marchand. All rights reserved.
-//
-
-import Foundation
