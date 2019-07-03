f14054ea068e0303fc6cba90b68dace811aa97fe
Eric Marchand <eric.marchand@4d.com>
2019-07-03 15:42:14 +0100

    [Mobile] Add missing file to highligth time and date row
    Jobs: redmine:105413
    [git-p4: depot-paths = "//depot/4eDimension/main/QMobile/QMobileUI/": change = 239252]

---
+++ Sources/Action/ActionUI/Eureka/DetailHighlightedRow.swift
@@ -0,0 +1,32 @@
+//
+//  DetailHighlightedRow.swift
+//  QMobileUI
+//
+//  Created by Eric Marchand on 03/07/2019.
+//  Copyright © 2019 Eric Marchand. All rights reserved.
+//
+
+import Foundation
+
+import Eureka
+
+/// Protocol to manage color when Highlighted.
+protocol DetailHighlightedRow: BaseRowType {
+    var isHighlighted: Bool { get }
+}
+
+extension DateRow: DetailHighlightedRow {}
+extension TimeIntervalRow: DetailHighlightedRow {}
+extension CountDownTimeRow: DetailHighlightedRow {}
+
+extension DetailHighlightedRow {
+
+    func updateHighlighted() {
+        guard let cell = baseCell else { return }
+        if self.isHighlighted {
+            cell.detailTextLabel?.textColor = cell.tintColor
+        } else {
+            cell.detailTextLabel?.textColor = UITableViewCell(style: .value1, reuseIdentifier: nil).detailTextLabel?.textColor // reset default color, better way is to get previous, store it and restore it here
+        }
+    }
+}
