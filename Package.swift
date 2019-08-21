3679dde987c3515aedb84891cc8700b3940d90dc
quentin marciset <quentin.marciset@4d.com>
2019-08-21 15:35:07 +0100

    [Mobile] Remove Result framework from SPM Package.swift #109872 #107928 - Result removed - Guitar added as contributors updated Package.swift - Couldn't upgrade Alamofire and Moya as they are only releasing alpha/beta version for the moment
    [git-p4: depot-paths = "//depot/4eDimension/main/QMobile/QMobileUI/": change = 240688]

--- Package.swift
+++ Package.swift
@@ -22,16 +22,15 @@ let package = Package(
         .package(url: "http://srv-git:3000/qmobile/QMobileDataStore.git" , .revision("HEAD")),
         .package(url: "http://srv-git:3000/qmobile/QMobileDataSync.git" , .revision("HEAD")),
 
-        .package(url: "https://github.com/phimage/CallbackURLKit.git" , .revision("HEAD")), // Requires macOS(.v10_14)
+        .package(url: "https://github.com/phimage/CallbackURLKit.git" , .revision("HEAD")),
         .package(url: "https://github.com/nvzqz/FileKit.git" , .revision("48b5ddb287f131a5c628badc819b880453f94449")), // HEAD
-        .package(url: "https://github.com/antitypical/Result.git" , from: "4.1.0"),
         .package(url: "https://github.com/DaveWoodCom/XCGLogger.git" , from: "7.0.0"),
         .package(url: "https://github.com/weichsel/ZIPFoundation.git" , from: "0.9.9"),
-        .package(url: "https://github.com/Thomvis/BrightFutures.git" , from: "8.0.1")
+        .package(url: "https://github.com/Thomvis/BrightFutures.git" , from: "8.0.1"),
+        .package(url: "https://github.com/ArtSabintsev/Guitar.git", from: "1.0.2")
         
 //        .package(url: "https://github.com/onevcat/Kingfisher.git" , .revision("68b7aa28a1d9f03ac00f2eeb0c522422dcd562bb")), // not for macOS
 //        .package(url: "https://github.com/phimage/ValueTransformerKit.git" , from: "1.2.0") // No Package.swift file
-//        .package(url: "https://github.com/ArtSabintsev/Guitar.git", .revision("7609e913619c606b7b1e3f251f18b906a1ef7272")), // Package.swift not updated
 //        .package(url: "https://github.com/SwiftKickMobile/SwiftMessages.git" , from: "7.0.0") // No Package.swift file
 //        .package(url: "https://github.com/xmartlabs/Eureka.git" , from: "5.0.0") // No Package.swift file
 //        .package(url: "https://github.com/devicekit/DeviceKit.git" , .revision("89452446badb4391899e989b8ae99c84488457f5")), // not for macOS
@@ -49,10 +48,10 @@ let package = Package(
                 "QMobileDataSync",
                 "CallbackURLKit",
                 "FileKit",
-                "Result",
                 "XCGLogger",
                 "ZIPFoundation",
-                "BrightFutures"
+                "BrightFutures",
+                "Guitar"
             ],
             path: "Sources"),
         .testTarget(
