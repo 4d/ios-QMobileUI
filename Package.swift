be6de106f711ba233566c55caca6212e398f4f74
quentin marciset <quentin.marciset@4d.com>
2019-09-12 11:03:01 +0100

    [Mobile] updating Package.swift files for QMobile packages #107928
    [git-p4: depot-paths = "//depot/4eDimension/main/QMobile/QMobileUI/": change = 241439]

--- Package.swift
+++ Package.swift
@@ -22,10 +22,10 @@ let package = Package(
         .package(url: "https://github.com/DaveWoodCom/XCGLogger.git" , from: "7.0.0"),
         .package(url: "https://github.com/weichsel/ZIPFoundation.git" , from: "0.9.9"),
         .package(url: "https://github.com/Thomvis/BrightFutures.git" , from: "8.0.1"),
-        .package(url: "https://github.com/devicekit/DeviceKit.git", from: "2.1.0"),
         .package(url: "https://github.com/ArtSabintsev/Guitar.git", from: "1.0.2"),
+        .package(url: "https://github.com/phimage/ValueTransformerKit.git" , from: "1.2.1"),
         // .package(url: "https://github.com/onevcat/Kingfisher.git" , from: "5.7.1"),
-        // .package(url: "https://github.com/phimage/ValueTransformerKit.git" , from: "1.2.1"),
+        // .package(url: "https://github.com/devicekit/DeviceKit.git", from: "2.1.0"),
         //.package(url: "https://github.com/SwiftKickMobile/SwiftMessages.git" , from: "7.0.0") // No Package.swift file
         // .package(url: "https://github.com/xmartlabs/Eureka.git" , from: "5.0.0"), // No Package.swift file
         // .package(url: "https://github.com/IBAnimatable/IBAnimatable.git", .revision("0776c5c099b308cd0cffe14f8cf89f0371153d03"))
@@ -44,9 +44,9 @@ let package = Package(
                 "XCGLogger",
                 "ZIPFoundation",
                 "BrightFutures",
-                "DeviceKit",
                 "Guitar",
-                // "ValueTransformerKit",
+                "ValueTransformerKit",
+                // "DeviceKit",
                 // "Kingfisher"
             ],
             path: "Sources"),
