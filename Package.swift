a6595c0b6f20bda627e349d4d75edd50e3fc829c
Eric Marchand <eric.marchand@4d.com>
2019-10-02 12:09:03 +0100

    [Mobile] Update some third party framework: Prephirences, DeviceKit, KingFisher
    [git-p4: depot-paths = "//depot/4eDimension/main/QMobile/QMobileUI/": change = 242332]

--- Package.swift
+++ Package.swift
@@ -24,8 +24,9 @@ let package = Package(
         .package(url: "https://github.com/Thomvis/BrightFutures.git" , from: "8.0.1"),
         .package(url: "https://github.com/ArtSabintsev/Guitar.git", from: "1.0.2"),
         .package(url: "https://github.com/phimage/ValueTransformerKit.git" , from: "1.2.1"),
+        .package(url: "https://github.com/phimage/Prephirences.git", from: "5.1.0"),
         // .package(url: "https://github.com/onevcat/Kingfisher.git" , from: "5.7.1"),
-        // .package(url: "https://github.com/devicekit/DeviceKit.git", from: "2.1.0"),
+        // .package(url: "https://github.com/devicekit/DeviceKit.git", from: "2.3.0"),
         //.package(url: "https://github.com/SwiftKickMobile/SwiftMessages.git" , from: "7.0.0") // No Package.swift file
         // .package(url: "https://github.com/xmartlabs/Eureka.git" , from: "5.0.0"), // No Package.swift file
         // .package(url: "https://github.com/IBAnimatable/IBAnimatable.git", .revision("0776c5c099b308cd0cffe14f8cf89f0371153d03"))
