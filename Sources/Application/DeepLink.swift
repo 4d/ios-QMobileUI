45416b8bf455cb4ca706d4fd02176727b9b8d300
Eric Marchand <eric.marchand@4d.com>
2020-07-15 09:02:09 +0100

    [Mobile] Allow to encode deeplink for app shortcut or maybe history of navigation #117618
    Jobs: redmine:117618
    [git-p4: depot-paths = "//depot/4eDimension/main/QMobile/QMobileUI/": change = 254041]

--- Sources/Application/DeepLink.swift
+++ Sources/Application/DeepLink.swift
@@ -1,12 +1,12 @@
 //
-//  File.swift
+//  DeepLink.swift
 //  QMobileUI
 //
 //  Created by phimage on 14/07/2020.
 //  Copyright © 2020 Eric Marchand. All rights reserved.
 //
 
-import Foundation
+import UIKit
 import SwiftyJSON
 
 public enum DeepLink {
@@ -16,7 +16,7 @@ public enum DeepLink {
     case settings
     case table(String)
     case record(String, Any)
-    case relation(String, Any, String)
+    case relation(String, Any, String) // ot change that to an history of deep link?
 
     // Return state from push notif informaton for instance
     static func from(_ userInfo: [AnyHashable: Any]) -> DeepLink? {
@@ -97,4 +97,89 @@ public enum DeepLink {
 
 public protocol DeepLinkable {
     var deepLink: DeepLink? { get }
+}
+
+import QMobileAPI // for AnyCodable
+
+extension DeepLink: Codable {
+
+    enum CodingKeys: CodingKey {
+        case main, mainNavigation, login, settings, table, record, relation
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let key = container.allKeys.first
+        switch key {
+        case .main:
+            self = .main
+        case .mainNavigation:
+            self = .mainNavigation
+        case .settings:
+            self = .settings
+        case .login:
+            self = .login
+        case .table:
+            var nestedContainer = try container.nestedUnkeyedContainer(forKey: .table)
+            let tableName = try nestedContainer.decode(String.self)
+            self = .table(tableName)
+        case .record:
+            var nestedContainer = try container.nestedUnkeyedContainer(forKey: .table)
+            let tableName = try nestedContainer.decode(String.self)
+            guard let primaryKeyValue = try nestedContainer.decode(AnyCodable.self).wrapped else {
+                throw DecodingError.dataCorrupted(
+                    DecodingError.Context(
+                        codingPath: container.codingPath,
+                        debugDescription: "Unabled to decode deepLink primary key value."
+                    )
+                )
+            }
+            self = .record(tableName, primaryKeyValue)
+        case .relation:
+            var nestedContainer = try container.nestedUnkeyedContainer(forKey: .relation)
+            let tableName = try nestedContainer.decode(String.self)
+            guard let primaryKeyValue = try nestedContainer.decode(AnyCodable.self).wrapped else {
+                throw DecodingError.dataCorrupted(
+                    DecodingError.Context(
+                        codingPath: container.codingPath,
+                        debugDescription: "Unabled to decode deepLink primary key value."
+                    )
+                )
+            }
+            let relationName = try nestedContainer.decode(String.self)
+            self = .relation(tableName, primaryKeyValue, relationName)
+        default:
+            throw DecodingError.dataCorrupted(
+                DecodingError.Context(
+                    codingPath: container.codingPath,
+                    debugDescription: "Unabled to decode deepLink."
+                )
+            )
+        }
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        switch self {
+        case .main:
+            try container.encode(true, forKey: .main)
+        case .mainNavigation:
+            try container.encode(true, forKey: .mainNavigation)
+        case .settings:
+            try container.encode(true, forKey: .settings)
+        case .login:
+            try container.encode(true, forKey: .login)
+        case .table(let tableName):
+            try container.encode(tableName, forKey: .table)
+        case .record(let tableName, let recordPrimaryKey):
+            var nestedContainer = container.nestedUnkeyedContainer(forKey: .record)
+            try nestedContainer.encode(tableName)
+            try nestedContainer.encode(AnyCodable(recordPrimaryKey))
+        case .relation(let tableName, let recordPrimaryKey, let relationName):
+            var nestedContainer = container.nestedUnkeyedContainer(forKey: .relation)
+            try nestedContainer.encode(tableName)
+            try nestedContainer.encode(AnyCodable(recordPrimaryKey))
+            try nestedContainer.encode(relationName)
+        }
+    }
 }
