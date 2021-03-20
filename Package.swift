5a09075bd018762609c51d131c67c69677d59f81
Eric Marchand <eric.marchand@4d.com>
2021-03-20 06:33:51 +0100

    Upgrade Kingfisher to 6.x
    [git-p4: depot-paths = "//depot/4eDimension/main/QMobile/QMobileUI/": change = 263337]

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         .package(url: "https://github.com/weichsel/ZIPFoundation.git", from: "0.9.9"),
         .package(url: "https://github.com/phimage/ValueTransformerKit.git", from: "1.2.3"),
         .package(url: "https://github.com/phimage/Prephirences.git", from: "5.1.0"),
-        .package(url: "https://github.com/onevcat/Kingfisher.git", from: "5.13.4"),
+        .package(url: "https://github.com/onevcat/Kingfisher.git", from: "6.2.1"),
 
 //         .package(url: "https://github.com/devicekit/DeviceKit.git", from: "2.3.0"), // no mac support
         .package(url: "https://github.com/phimage/DeviceKit.git", .branch("feature/macos")), // https://github.com/devicekit/DeviceKit/pull/217
