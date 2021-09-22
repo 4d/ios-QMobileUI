be6c3abbc1f7d5f9c7b83180709626c87d382502
Eric Marchand <eric.marchand@4d.com>
2021-09-22 08:39:23 +0100

    [iOS] Suport relation in record formatting "%lien1....lienN.field%"  #130722
    Jobs: redmine:128195
    [git-p4: depot-paths = "//depot/4eDimension/main/QMobile/QMobileUI/": change = 271010]

--- Sources/Bind/RecordFormatter+QMobileDataStore.swift
+++ Sources/Bind/RecordFormatter+QMobileDataStore.swift
@@ -22,6 +22,29 @@ extension RecordFormatter {
 private struct DataStoreFieldNodeInfo: FieldNodeInfo {
     let tableInfo: DataStoreTableInfo
     func fieldName(name: String) -> String? {
-        return tableInfo.fields.filter({ $0.originalName == name}).first?.name
+        if name.contains(".") {
+            var coreDataNames: [String] = []
+            var keyPath = name.split(separator: ".")
+            guard let last = keyPath.popLast() else {
+                logger.verbose("Cannot find in database \(name) from \(tableInfo.name)")
+                return nil
+            }
+            var keyTable: DataStoreTableInfo? = tableInfo
+            for key in keyPath {
+                guard let relation = keyTable?.relationships.first(where: { $0.originalName == key}) else {
+                    logger.debug("Cannot find in database \(name) from \(tableInfo.name), when looking for relation \(key)")
+                    return nil
+                }
+                coreDataNames.append(relation.name)
+                keyTable = relation.destinationTable
+            }
+            guard let field = keyTable?.fields.first(where: { $0.originalName == last}) else {
+                logger.debug("Cannot find in database \(name) from \(tableInfo.name), when looking for field \(last) into \(keyTable?.name ?? "")")
+                return nil
+            }
+            coreDataNames.append(field.name)
+            return coreDataNames.joined(separator: ".")
+        }
+        return tableInfo.fields.first(where: { $0.originalName == name})?.name
     }
 }
