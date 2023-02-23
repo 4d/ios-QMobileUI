30a76e386f0ae6b15f87255509065405c950b7eb
eric.marchand <eric.marchand@4d.com>
2023-02-23 08:07:02 +0100

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -15,11 +15,11 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/phimage/CallbackURLKit.git", .revision("HEAD")),
-        .package(url: "https://github.com/nvzqz/FileKit.git", from: "6.0.0"),
+        .package(url: "https://github.com/nvzqz/FileKit.git", from: "6.1.0"),
         .package(url: "https://github.com/DaveWoodCom/XCGLogger.git", from: "7.0.0"),
         .package(url: "https://github.com/weichsel/ZIPFoundation.git", from: "0.9.14"),
-        .package(url: "https://github.com/phimage/ValueTransformerKit.git", from: "1.2.3"),
-        .package(url: "https://github.com/phimage/Prephirences.git", from: "5.3.1"),
+        .package(url: "https://github.com/phimage/ValueTransformerKit.git", from: "1.2.4"),
+        .package(url: "https://github.com/phimage/Prephirences.git", from: "5.4.0"),
         .package(url: "https://github.com/onevcat/Kingfisher.git", from: "6.2.1"),
 
 //        .package(url: "https://github.com/SwiftKickMobile/SwiftMessages.git", from: "7.0.0"), // No Package.swift file
