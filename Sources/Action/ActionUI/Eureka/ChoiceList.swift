d43b85bcfe0504be26a8c2c4dccda39d5add109b
eric.marchand <eric.marchand@4d.com>
2023-11-08 06:00:20 +0100

    Allow to set a coredata filter on datasource choicelist using coredata and key filter.ios

--- Sources/Action/ActionUI/Eureka/ChoiceList.swift
+++ Sources/Action/ActionUI/Eureka/ChoiceList.swift
@@ -139,7 +139,10 @@ struct ChoiceList {
 
             var fetchedRequest = DataStoreFactory.dataStore.fetchRequest(tableName: dataClass, sortDescriptors: sortDescriptors)
             if let filter = dataSource["filter"] as? String {
-                fetchedRequest.predicate = NSPredicate(format: filter) // must be coredata but if orda?
+                fetchedRequest.predicate = NSPredicate(format: filter) // must be coredata but if orda? (need a converter)
+            }
+            else if let filters = dataSource["filter"] as? [String: Any], let filter = (filters["ios"] ?? filters["iOS"]) as? String {
+                fetchedRequest.predicate = NSPredicate(format: filter) // must be coredata
             }
             var records: [Record] = []
             var optionsFromRecords: [ChoiceListItem] = []
